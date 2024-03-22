e24b04f5d202ee925f8e25bcbdbde73a52d84b01
Okorie GoodGod <112786412+GuddyTech@users.noreply.github.com>
2024-03-22 15:44:50 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,22 @@
+# variables.tf
+
+variable "username" {
+  description = "The username of the Azure AD user"
+  type        = string
+}
+
+variable "role_definition_id" {
+  description = "The ID of the role definition to assign to the user"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group to deploy the user-assigned managed identity"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where the resources will be provisioned"
+  type        = string
+}
 
